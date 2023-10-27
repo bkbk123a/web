a53004c935ec010965e3866115ebe9a7d77304e8
bkbk123a <qjarud32@naver.com>
2023-10-28 04:06:37 +0900

    아이템 관련 Repository 정의

---
+++ web/src/main/java/com/example/web/jpa/repository/item/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.web.jpa.repository.item;
+
+import com.example.web.jpa.entity.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
---
+++ web/src/main/java/com/example/web/jpa/repository/item/UserItemLogRepository.java
@@ -0,0 +1,7 @@
+package com.example.web.jpa.repository.item;
+
+import com.example.web.jpa.entity.item.UserItemLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserItemLogRepository extends JpaRepository<UserItemLog, Long> {
+}
