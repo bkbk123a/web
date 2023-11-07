c5217b34d0935f41245c1982c2e3f956479b0679
beomkyung1477 <beomkyung1477@com2us.com>
2023-11-07 18:22:22 +0900

    인터셉터 패턴 변경

--- web/src/main/java/com/example/web/config/WebMvcConfig.java
+++ web/src/main/java/com/example/web/config/WebMvcConfig.java
@@ -17,7 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     registry.addInterceptor(sessionInterceptor)
         .order(1)                 // 인터셉터 호출 순위. 낮을수록 먼저 호출
-        .addPathPatterns("/**")    // 인터셉터 적용할 URL 패턴
+        .addPathPatterns("/attend/**")
+        .addPathPatterns("/item/**")
+        .addPathPatterns("/login/**")
+        .addPathPatterns("/oauth/**")
+        .addPathPatterns("/user/**")
         .excludePathPatterns("/error"); // 인터셉터 제외할 패턴 지정
   }
 }
