5df695a7cfc3e64721de18e34cbef67fba575c23
beomkyung1477 <beomkyung1477@com2us.com>
2023-11-09 11:19:01 +0900

    인터셉터 호출 관련 내용 변경

--- web/src/main/java/com/example/web/config/WebMvcConfig.java
+++ web/src/main/java/com/example/web/config/WebMvcConfig.java
@@ -17,11 +17,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     registry.addInterceptor(sessionInterceptor)
         .order(1)                 // 인터셉터 호출 순위. 낮을수록 먼저 호출
-        .addPathPatterns("/attend/**")
-        .addPathPatterns("/item/**")
-        .addPathPatterns("/login/**")
-        .addPathPatterns("/oauth/**")
-        .addPathPatterns("/user/**")
-        .excludePathPatterns("/error"); // 인터셉터 제외할 패턴 지정
+        .addPathPatterns("/**");
   }
 }
