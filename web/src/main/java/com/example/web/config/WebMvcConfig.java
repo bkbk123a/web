244f9d49e38d395ba16d1fadd7376e642924dd3a
beomkyung1477 <beomkyung1477@com2us.com>
2023-11-13 17:48:32 +0900

    인터셉터 exclude 수정

--- web/src/main/java/com/example/web/config/WebMvcConfig.java
+++ web/src/main/java/com/example/web/config/WebMvcConfig.java
@@ -17,6 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     registry.addInterceptor(sessionInterceptor)
         .order(1)                 // 인터셉터 호출 순위. 낮을수록 먼저 호출
-        .addPathPatterns("/**");
+        .addPathPatterns("/attend/**")
+        .addPathPatterns("/product/**")
+        .addPathPatterns("/login/**")
+        .addPathPatterns("/oauth/**")
+        .addPathPatterns("/user/**")
+        .excludePathPatterns("/error"); // 인터셉터 제외할 패턴 지정
   }
 }
