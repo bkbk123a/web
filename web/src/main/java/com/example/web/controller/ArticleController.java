a2c8a6447074ba24cebf4d7ba8491dd419b8bb1e
beomkyung1477 <beomkyung1477@com2us.com>
2023-12-08 17:05:03 +0900

    게시글(댓글) 기본 골격

--- web/src/main/java/com/example/web/controller/ArticleController.java
+++ web/src/main/java/com/example/web/controller/ArticleController.java
@@ -1,18 +1,31 @@
 package com.example.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+  @Operation(summary = "게시글 전체 조회")
   @GetMapping
-  public String articles(ModelMap map) {
+  public String getArticles(ModelMap map) {
     map.addAttribute("articles", List.of());
 
     return "articles/index";
+  }
+
+  @Operation(summary = "게시글 상세 조회")
+  @GetMapping("/{articleId}")
+  public String getArticle(@PathVariable Long articleId, ModelMap map) {
+    map.addAttribute("article", "article"); // TODO: 실제 데이터
+    map.addAttribute("articleComments", List.of());
+
+    return "articles/detail";
   }
 }
